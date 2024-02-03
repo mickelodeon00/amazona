b059656930f4c09bf881dde6dc2f52f524e17f75
Micheal <michealakingbade00>
2024-02-03 12:33:24 +0100

    starting-amazona
    starting the amazona web page

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">amazona</a>
+      </header>
+      <main>Hello World</main>
+    </div>
+  );
+}
+
+export default App;
