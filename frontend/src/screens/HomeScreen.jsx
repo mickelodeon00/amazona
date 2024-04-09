2fb7790d56effc0ee91f5605849aaf5b6daccd46
Micheal <michealakingbade00>
2024-04-09 22:10:36 +0100

    Remove use-reducer-logger from FE package.json

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useReducer } from 'react';
 import axios from 'axios';
-import logger from 'use-reducer-logger';
+// import logger from 'use-reducer-logger';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Product from '../components/Product';
@@ -21,7 +21,7 @@ const reducer = (state, action) => {
 };
 
 const HomeScreen = () => {
-  const [{ products, loading, error }, dispatch] = useReducer(logger(reducer), {
+  const [{ products, loading, error }, dispatch] = useReducer(reducer, {
     products: [],
     loading: true,
     error: '',
